31c718bc1c152262df86a74b0dd42c9aecb4542d
darshan4694 <darshan.shah4694@gmail.com>
2017-08-01 21:13:05 -0500

    Working with count() insted of find()

--- mongodb_find.js
+++ mongodb_find.js
@@ -6,33 +6,33 @@ MongoClient.connect('mongodb://localhost:27017/ToDoApp', (err, db) => {
         return console.log("Unable to connect to the databse ", err);
     }
 
-    db.collection('Todos').find().toArray().then((docs) => {
+    db.collection('Todos').find().count().then((count) => {
         console.log('------------------------------------');
         console.log('----------------find all------------');
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2));
+        console.log('Todos count: ', count);
+        //console.log(JSON.stringify(docs, undefined, 2));
     }, (err) => {
         console.log("Unable to fetch data", err);
     });
 
-    db.collection('Todos').find({ completed: true }).toArray().then((docs) => {
+    db.collection('Todos').find({ completed: true }).count().then((count) => {
         console.log('------------------------------------');
         console.log('------------------------------------');
         console.log('-------find by property name--------');
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2));
+        console.log('Todos count: ', count);
+        // console.log(JSON.stringify(docs, undefined, 2));
     }, (err) => {
         console.log("Unable to fetch data", err);
     });
 
     db.collection('Todos').find({
         _id: ObjectID("598088858db73042a1a09597")
-    }).toArray().then((docs) => {
+    }).count().then((count) => {
         console.log('------------------------------------');
         console.log('------------------------------------');
         console.log('------find by id--------------------');
-        console.log('Todos');
-        console.log(JSON.stringify(docs, undefined, 2));
+        console.log('Todos count: ', count);
+        // console.log(JSON.stringify(docs, undefined, 2));
     }, (err) => {
         console.log("Unable to fetch data", err);
     });
